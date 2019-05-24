86e8cf4f8956a50cbb2cc80a0f1add663be8bc1d
Robin <robin@naturewake.com>
2019-05-24 15:26:21 +0800

    remove node_modules from git

--- lib/prod/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as StoragePackage from './storage';
-export { PostgresSQL, PostgresServerSettings, PostgresDatabaseStructure, PostgresTableStructure, StorageType, StorageSettings, StorageBaseClass } from './storage';
-export declare const storage: typeof StoragePackage;
-//# sourceMappingURL=index.d.ts.map
--- lib/prod/storage/base.d.ts
+++
@@ -1,5 +0,0 @@
-export declare abstract class StorageBase {
-    abstract get(table: string, identities?: any): Promise<any>;
-    abstract set(table: string, obj: any, identities?: any): Promise<any>;
-}
-//# sourceMappingURL=base.d.ts.map
