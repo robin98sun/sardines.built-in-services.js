fc01b18ca64b99633aeb755e3c427078461f6155
Robin <robin@naturewake.com>
2020-04-08 11:50:24 +0800

    "sardines publisher automatic commit"

--- lib/gateway/nginx.d.ts
+++ lib/gateway/nginx.d.ts
@@ -1,2 +1,5 @@
+export { NginxProtocol } from './nginx.sardine';
+export { NginxConfig } from './nginx.sardine';
 export declare const echo: (msg: string) => string | Promise<unknown>;
+export declare const config: (configData?: any) => Promise<any>;
 //# sourceMappingURL=nginx.d.ts.map
