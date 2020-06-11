015f18b5f0249a8a9497dba169b07c086fd9bbc6
Robin <robin@naturewake.com>
2020-06-11 17:46:32 +0800

    sardine project automatic save point

--- lib/gateway/nginx.d.ts
+++
@@ -1,5 +0,0 @@
-export { NginxProtocol } from './nginx.sardine';
-export { NginxConfig } from './nginx.sardine';
-export declare const echo: (msg: string) => string | Promise<unknown>;
-export declare const config: (configData?: any) => Promise<any>;
-//# sourceMappingURL=nginx.d.ts.map
