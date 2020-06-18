05956bc4a124110335c8aef214f64bb1c8ae15f3
Robin <robin@naturewake.com>
2020-06-18 15:12:32 +0800

    sardine project automatic save point

--- src/access_point/nginx/index.ts
+++ src/access_point/nginx/index.ts
@@ -1,34 +1,15 @@
-import * as origin from './index.sardine'
-import { Core } from 'sardines-core'
+import { NginxReverseProxy } from './nginx_reverse_proxy'
+let proxy: NginxReverseProxy|null = null
 
-export const setup = async (ipaddr: string = '0.0.0.0', port: number = 80, auth: any, nginxConfigFilePath:string = '/etc/nginx/nginx.conf', nginxConfigDir: string = '/etc/nginx/conf.d/') => {
-    if (Core.isRemote('sardines-built-in-services', '/access_point/nginx', 'setup')) {
-        return await Core.invoke({
-            identity: {
-                application: 'sardines-built-in-services',
-                module: '/access_point/nginx',
-                name: 'setup',
-                version: '*'
-            },
-            entries: []
-        }, ipaddr, port, auth, nginxConfigFilePath, nginxConfigDir)
-    } else {
-        return await origin.setup(ipaddr, port, auth, nginxConfigFilePath, nginxConfigDir)
-    }
+export const setup = async (
+  ipaddr: string = '0.0.0.0', port: number = 80, auth: any, 
+  nginxConfigFilePath:string = '/etc/nginx/nginx.conf', 
+  nginxConfigDir: string = '/etc/nginx/conf.d/'
+) => {
+  proxy = new NginxReverseProxy(ipaddr, port, auth, nginxConfigFilePath, nginxConfigDir)
+  return await proxy.start()
 }
 
 export const info = async () => {
-    if (Core.isRemote('sardines-built-in-services', '/access_point/nginx', 'info')) {
-        return await Core.invoke({
-            identity: {
-                application: 'sardines-built-in-services',
-                module: '/access_point/nginx',
-                name: 'info',
-                version: '*'
-            },
-            entries: []
-        })
-    } else {
-        return await origin.info()
-    }
+  return (proxy||{}).info
 }
