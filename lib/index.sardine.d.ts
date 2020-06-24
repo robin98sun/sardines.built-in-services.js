d3e12353c231d2bc65f2558287233b9f6355a1b5
Robin <robin@naturewake.com>
2020-06-24 09:45:55 +0800

    sardine project automatic save point

--- lib/index.sardine.d.ts
+++
@@ -1,3 +0,0 @@
-export { PostgreSQL, PostgresServerSettings, PostgresDatabaseStructure, PostgresTableStructure, StorageType, StorageSettings, Storage, setup as setupStorage, RedisCache, RedisServerSettings, RedisDataType, RedisOperationOptions } from './storage/index';
-export { TempleteAccount as PostgresTempleteAccount, Account, Token } from './templetes/postgresql_templete_account';
-//# sourceMappingURL=index.sardine.d.ts.map
