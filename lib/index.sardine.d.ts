38088436d5f56485451e0c5b4185d34d74927147
Robin <robin@naturewake.com>
2020-06-24 09:39:42 +0800

    "sardines publisher automatic commit"

---
+++ lib/index.sardine.d.ts
@@ -0,0 +1,3 @@
+export { PostgreSQL, PostgresServerSettings, PostgresDatabaseStructure, PostgresTableStructure, StorageType, StorageSettings, Storage, setup as setupStorage, RedisCache, RedisServerSettings, RedisDataType, RedisOperationOptions } from './storage/index';
+export { TempleteAccount as PostgresTempleteAccount, Account, Token } from './templetes/postgresql_templete_account';
+//# sourceMappingURL=index.sardine.d.ts.map
