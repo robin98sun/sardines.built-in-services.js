6d9461ef4a8d6ca3c92dafbbe5ace7dbdabe9166
Robin <robin@naturewake.com>
2020-06-24 10:34:05 +0800

    "sardines publisher automatic commit"

---
+++ lib/sardines/index.sardine.d.ts
@@ -0,0 +1,28 @@
+export declare const drivers: {
+    [key: string]: any;
+};
+export declare const sardinesConfig: {
+    application: string;
+    platform: string;
+    exeDir: string;
+    srcRootDir: string;
+    sardinesDir: string;
+    remoteServices: {};
+    repositoryEntries: {
+        providerInfo: {
+            protocol: string;
+            host: string;
+            port: number;
+            root: string;
+            driver: string;
+        };
+        user: string;
+        password: string;
+    }[];
+    drivers: {
+        name: string;
+        locationType: string;
+        protocols: string[];
+    }[];
+};
+//# sourceMappingURL=index.sardine.d.ts.map
